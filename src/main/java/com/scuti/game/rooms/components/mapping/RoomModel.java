3083785453576cbcbcb46a3d3d350eca52a6e4c0
qgarot <quentingarot06@gmail.com>
2024-01-20 15:06:54 +0100

    Update room model

--- src/main/java/com/scuti/game/rooms/components/mapping/RoomModel.java
+++ src/main/java/com/scuti/game/rooms/components/mapping/RoomModel.java
@@ -8,6 +8,10 @@ public class RoomModel {
     private int doorRotation;
     private String heightmap;
     private boolean clubOnly;
+    private int mapSizeX;
+    private int mapSizeY;
+    private int[][] tileStates;
+    private int[][] tileHeights;
 
     public RoomModel(String name, int doorX, int doorY, int doorZ, int doorRotation, String heightmap, boolean clubOnly) {
         this.name = name;
@@ -49,6 +53,22 @@ public class RoomModel {
         return clubOnly;
     }
 
+    public int getMapSizeX() {
+        return mapSizeX;
+    }
+
+    public int getMapSizeY() {
+        return mapSizeY;
+    }
+
+    public int[][] getTileStates() {
+        return tileStates;
+    }
+
+    public int[][] getTileHeights() {
+        return tileHeights;
+    }
+
     private int[][] generate_array(int x, int y) {
         int[][] matrix = new int[y][x];
 
@@ -63,25 +83,25 @@ public class RoomModel {
 
     public void parse() {
         String[] lines = this.getHeightmap().split("\r\n");
-        int mapSizeY = lines.length;
-        int mapSizeX = lines[0].length();
-        int[][] tileStates = this.generate_array(mapSizeX, mapSizeY);
-        int[][] tileHeights = this.generate_array(mapSizeX, mapSizeY);
+        this.mapSizeY = lines.length;
+        this.mapSizeX = lines[0].length();
+        this.tileStates = this.generate_array(mapSizeX, mapSizeY);
+        this.tileHeights = this.generate_array(mapSizeX, mapSizeY);
         String temporaryHeightmap = "";
         String line;
         Character tile;
 
-        for (int y = 0; y < mapSizeY; y++) {
+        for (int y = 0; y < this.getMapSizeY(); y++) {
             line = lines[y];
-            for (int x = 0; x < mapSizeX; x++) {
+            for (int x = 0; x < this.getMapSizeX(); x++) {
                 tile = line.charAt(x);
 
                 try {
-                    tileHeights[y][x] = Integer.parseInt(String.valueOf(tile));
-                    tileStates[y][x] = 1;
+                    this.tileHeights[y][x] = Integer.parseInt(String.valueOf(tile));
+                    this.tileStates[y][x] = 1;
                 } catch (NumberFormatException e) {
-                    tileHeights[y][x] = 0;
-                    tileStates[y][x] = 0;
+                    this.tileHeights[y][x] = 0;
+                    this.tileStates[y][x] = 0;
                 }
 
                 if (x == this.getDoorX() && y == this.getDoorY()) {
