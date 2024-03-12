ec264734b4311f7495089e9ca7e87734275c6f62
qgarot <quentingarot06@gmail.com>
2024-03-12 21:56:42 +0100

    Create generic DAO interface

---
+++ src/main/java/com/scuti/api/dao/Dao.java
@@ -0,0 +1,25 @@
+package com.scuti.api.dao;
+
+import java.util.List;
+
+public interface Dao<T> {
+
+    /**
+     * Get the content of the table
+     * @return list of corresponding instances
+     */
+    List<T> getAll();
+
+    /**
+     * Insert data corresponding to the given T object
+     * @param t: instance to insert in the database
+     */
+    void create(T t);
+
+    /**
+     * Update data corresponding to the given T object
+     * @param t: instance to save in the database
+     */
+    void save(T t);
+
+}
